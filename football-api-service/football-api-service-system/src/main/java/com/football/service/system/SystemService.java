c140498691daa23c474574036c955281c71119e4
zhaofangqiang <zhaofangqiang123>
2017-10-20 17:47:24 +0800

    adduserclass

---
+++ football-api-service/football-api-service-system/src/main/java/com/football/service/system/SystemService.java
@@ -0,0 +1,11 @@
+package com.football.service.system;
+
+/**
+ * Title: ${file_name}
+ * Description: TODO
+ * author:zhaofq
+ * date: 2017 10 ${date} ${time}
+ * version: V1.0
+ */
+public class SystemService {
+}
---
+++ football-api-service/football-api-service-user/src/java/com/football/service/user/UserService.java
@@ -0,0 +1,11 @@
+package com.football.service.user;
+
+/**
+ * Title: ${file_name}
+ * Description: TODO
+ * author:zhaofq
+ * date: 2017 10 ${date} ${time}
+ * version: V1.0
+ */
+public interface UserService {
+}
